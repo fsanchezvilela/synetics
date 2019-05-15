0ea17ece4f9f64d62d88448b03c8b913aca0593b
Fernando <f.sanchezvilela@gmail.com>
2019-05-15 22:20:08 +0200

    Technical Skill Section

--- portafolio/js/script.js
+++ portafolio/js/script.js
@@ -21,4 +21,43 @@ $(document).ready(function() {
     startDelay: 1400,
     showCursor: false
   });
+  //init owl-carousel
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      938: {
+        items: 4
+      }
+    }
+  });
+
+  var skillsTopOffset = $('.skillsSection').offset().top;
+
+  $(window).scroll(function() {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 300) {
+      $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find('.percent')
+            .text(Math.round(percent));
+        }
+      });
+    }
+  });
 });
